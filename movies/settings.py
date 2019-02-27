ea89a3e5f17b89ab9c423958ef390e1df1af8a15
Łukasz Wieczorek <wieczorek1990@gmail.com>
2019-02-27 16:16:33 +0100

    Add heroku packages

--- movies/settings.py
+++ movies/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,3 +133,6 @@ OMDB_API_KEY = os.environ.get('OMDB_API_KEY')
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
 }
+
+
+django_heroku.settings(locals())
